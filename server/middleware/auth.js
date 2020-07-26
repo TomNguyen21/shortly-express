669ee0c126dcc5895338cbea6f6736f6d91f1aa0
Jaycie Kassahn <jckassahn@gmail.com>
2020-07-25 20:42:07 -0700

    Stub out some createSession functionality

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,20 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  // if no cookie
+  if (!req.headers.cookie) {
+    // use models.Sessions.create to create a session row
+    models.Sessions.create()
+    // then, we use insertId to get unique hash
+    .then((response) => console.log(response));
+    // then, add unique hash to response header
+    // then, if user data exists
+      // retrieve user id
+      // then, add user data to session object on the request
+      // add user id to session table for current session
+      // then next?
+  } // otherwise
+      // stuff to do if there is a cookie
 };
 
 /************************************************************/
